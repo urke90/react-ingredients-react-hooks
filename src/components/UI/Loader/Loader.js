04190298f5333454edce2e6fd9fbccdb6ad8d96b
urke90 <urosbijelic90@gmail.com>
2020-11-13 13:39:08 +0100

    implement loader when adding ingredient

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Loader.css";
+
+const Loader = () => (
+  <div className="lds-ring">
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
